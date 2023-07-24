2c257cd460b9978859c1e47d2d2ac2a897d307a3
dim <nochev1@mail.ru>
2023-07-24 20:34:28 +0300

    env host db

--- core/settings.py
+++ core/settings.py
@@ -8,7 +8,7 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
+DEBUG = env('DEBUG')
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 
 
