7cc671c6850d6fccfb8bc5903c63aa7de8714c21
dim <nochev1@mail.ru>
2023-07-28 13:57:26 +0300

    mptt

--- core/settings.py
+++ core/settings.py
@@ -8,7 +8,7 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = env('DEBUG')
+DEBUG = env('DEBUG') == 'True'
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 
 
