e1616e23d4988f228b46dfe7341608967d02a24e
dim <nochev1@mail.ru>
2023-07-28 22:06:47 +0300

    footer resume

--- resume/models.py
+++ resume/models.py
@@ -95,13 +95,10 @@ class Project(models.Model):
         super().save(*args, **kwargs)
         delete_cache(self._meta.model_name)
         if self.image:
-            print('w')
             image = Image.open(self.image.path)
-            print('w1')
             if image.height > 270 or image.width > 270:
                 resize = (270, 270)
                 image.thumbnail(resize)
-                print('w2')
                 image.save(self.image.path)
 
 
