50100daa794a57086d5ba7c75506342acfdacebe
dim <nochev1@mail.ru>
2023-07-29 03:05:17 +0300

    update url

--- mptt_blog/models.py
+++ mptt_blog/models.py
@@ -14,8 +14,8 @@ PRIVAT_CHOICES = (
     (NO, "Только я"),
 )
 
-def category_author_default_post(*args, **kwargs):
 
+def category_author_default_post(*args, **kwargs):
     User = get_user_model()
     return User.objects.filter(is_superuser=True).order_by('id').first()
 
@@ -34,6 +34,7 @@ class Post(models.Model):
     is_fav = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
 
@@ -42,15 +43,31 @@ class Post(models.Model):
         verbose_name_plural = 'Посты'
 
     def get_absolute_url(self):
-
         return reverse('mptt_blog_urls:post_detail', kwargs={'slug_cat': self.category.url, 'pk': self.pk})
-
-
 
 
 def category_author_default(*args, **kwargs):
     User = get_user_model()
     return User.objects.filter(is_superuser=True).order_by('id').first()
+
+
+def update_branch_urls(obj, old_url):
+    update_list = []
+    for children in obj.get_descendants(include_self=False):
+        update_list.append(
+            Category(
+                id=children.id,
+                url=build_url(children.url, old_url, obj.url)
+            )
+        )
+    Category.objects.bulk_update(
+        update_list,
+        fields=['url']
+    )
+
+
+def build_url(children_url, old_url, obj_url):
+    return children_url.replace(old_url, obj_url, 1)
 
 
 class Category(MPTTModel):
@@ -63,24 +80,25 @@ class Category(MPTTModel):
     url = models.TextField()
 
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        if self.url:
-            c = Category.objects.get(pk=self.pk)
-            if c.title != self.title:
-                self.build_url()
-                for children in self.get_descendants(include_self=False):
-                    children.build_url()
-
+        update_branch = False
+        old_url = None
         if not self.url:
-            self.build_url()
+            self.url = self.build_url()
+        elif slugify(self.title) != self.url.split('/')[-1]:
+            update_branch = True
+            old_url = self.url
+            self.url = self.build_url()
+        super().save(*args, **kwargs)
+        if update_branch:
+            update_branch_urls(self, old_url)
 
     def build_url(self):
         title = slugify(self.title)
         if self.parent:
-            self.url = self.parent.url + '/' + title
+            url = self.parent.url + '/' + title
         else:
-            self.url = title
-        self.save()
+            url = title
+        return url
 
     class MPTTMeta:
         order_insertion_by = ['title']
