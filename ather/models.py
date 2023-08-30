32be21e100fb5480311ceda738ebd14a92eafc79
dim <nochev1@mail.ru>
2023-08-30 15:03:55 +0300

    resume api

--- ather/models.py
+++ ather/models.py
@@ -5,11 +5,12 @@ from ckeditor.fields import RichTextField
 from django.db import models
 from pytils.translit import slugify
 
-
+def time_time_now(date):
+    return timezone.localtime(date).strftime("%Y-%m-%d %H:%M:%S")
 class Person(models.Model):
     name = models.CharField('Персона', max_length=40, blank=False)
     slug = models.SlugField('slug', default='', blank=False)
-    visit_password = models.CharField('Пароль для просмотра', max_length=10, blank=True)
+    visit_password = models.CharField('Пароль для просмотра', max_length=10, blank=False)
     text1 = RichTextField('Текс1', blank=True)
     text2 = RichTextField('Текс2', blank=True)
     video_link = models.URLField('Ссылка на видео', blank=True)
@@ -22,10 +23,10 @@ class Person(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.name} Создан: {timezone.localtime(self.date).strftime("%Y-%m-%d %H:%M:%S")} '
+        return f'{self.name} Создан: {time_time_now(self.date)} '
 
     def save(self, *args, **kwargs):
-        self.slug = f'{self.slug}date{ slugify(timezone.localtime(self.date).strftime("%Y-%m-%d %H:%M:%S"))}'
+        self.slug = f'{self.slug}date{time_time_now(self.date)}'
         super().save(*args, **kwargs)
 
 
